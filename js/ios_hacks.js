53a937a4274d41fc974b7b31555ef8cb13f63d00
Herman Chau <hchau@stanford.edu>
2015-09-10 21:12:53 -0700

    Move ios hacks to separate file. Also another audio fix.

---
+++ js/ios_hacks.js
@@ -0,0 +1,14 @@
+/**
+ * So that this actually runs on ios
+ */
+var performance = window.performance ? window.performance : window.Date;
+var AudioContext = window.AudioContext ? window.AudioContext : window.webkitAudioContext;
+document.addEventListener('touchstart', function(event) {
+  var dummy_context = new AudioContext();
+  /* EXTREMELY DUMB HACK TO GET AUDIO WORKING ON IOS */
+	var buffer = dummy_context.createBuffer(1, 1, 22050);
+	var source = dummy_context.createBufferSource();
+	source.buffer = buffer;
+	source.connect(audio_context.destination);
+	source.noteOn(0);
+}, false);
